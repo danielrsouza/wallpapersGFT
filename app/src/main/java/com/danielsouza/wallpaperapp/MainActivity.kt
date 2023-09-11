7f76a443741ba18770f3a9cd09d21035176a5b19
Daniel Souza <48072832+danielrsouza@users.noreply.github.com>
2023-09-11 17:20:37 -0300

    inicializando o projeto
    inicializando o projeto

---
+++ app/src/main/java/com/danielsouza/wallpaperapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.danielsouza.wallpaperapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gft.learnanddevelopment.academy.kotlin_wallpaperproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
